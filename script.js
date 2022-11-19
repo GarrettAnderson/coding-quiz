2b20b455f8fbcb7bca0f5d65d85c205e6dcff7aa
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-19 15:55:32 -0500

    started the pseudo code in the JS section. added style of hide to display none the various containers

---
+++ script.js
@@ -0,0 +1,6 @@
+// on page load, only begin quiz container is shown
+
+// When user clicks on the 'start quiz' button
+    // the begin quiz container is hidden 
+    // the question container section is shown to user
+    // one question is shown to the user at a time
