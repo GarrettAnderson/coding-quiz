245779934e9d73ba70a46c073d9ecfed817d1569
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-19 16:40:04 -0500

    added class to the start quiz button. added questions array with a few questions. created a startQuiz function and beginning the login. created startTimer function with console.log message as placeholder

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
 const questions = [
-    
+    {
+        number: 1,
+        question: "Commonly used data types Do Not include:",
+        answer: "alerts",
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ]
+    },
+    {
+        number: 2,
+        question: "The condition in if/else statements is enclosed in:",
+        answer: "parenthesis",
+        options: [
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square brackets"
+        ]
+    },
+    {
+        number: 3,
+        question: "Arrays in JavaScript can be used to store:",
+        answer: "all of the above",
+        options: [
+            "numbers",
+            "strings",
+            "booleans",
+            "all of the above"
+        ]
+    }
 ]
+ 
+const beginQuizContainer = document.querySelector(".begin-quiz-container")
+const startQuizBtn = document.querySelector(".startQuizBtn")
+const questionContainer = document.querySelector(".question-container")
 
+function startTimer() {
+    console.log('start timer')
+}
 
-// on page load, only begin quiz container is shown
-
-// when user clicks start
+function startQuiz() {
 
 // When user clicks on the 'start quiz' button
-    // the begin quiz container is hidden 
+    // the begin quiz container is hidden.
+    beginQuizContainer.classList.add("hide")
     // the question container section is shown to user
+    questionContainer.classList.remove("hide")
+
+    // the timer is started
+    startTimer()
+    
     // one question is shown to the user at a time
+    // when user clicks on an answer choice, 
+        //the choice is evaluated to be correct or incorrect
+        // the user moves on to the next question in the sequence
+
+
+}
+
+
+
+startQuizBtn.addEventListener("click", startQuiz)
