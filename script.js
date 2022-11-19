0be746dfe2e19c963b3859a48debcf857f600b90
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-19 16:13:42 -0500

    adding pseudo code to the js file

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const questions = [
 
 // on page load, only begin quiz container is shown
 
+// when user clicks start
+
 // When user clicks on the 'start quiz' button
     // the begin quiz container is hidden 
     // the question container section is shown to user
