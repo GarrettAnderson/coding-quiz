38913f6719e50876cd6cd555ca3132a751f699a0
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-19 20:02:57 -0500

    able to get one question to render to the user. able to click on an answer and get it logged to the console.

--- script.js
+++ script.js
@@ -37,13 +37,27 @@ const questions = [
 const beginQuizContainer = document.querySelector(".begin-quiz-container")
 const startQuizBtn = document.querySelector(".startQuizBtn")
 const questionContainer = document.querySelector(".question-container")
+let questionsIndex
 
 function startTimer() {
     console.log('start timer')
 }
 
-function startQuiz() {
+function chooseAsAnswer(answerChoice, index) {
+    console.log(answerChoice.target)
+    //the choice is evaluated to be correct or incorrect
 
+    const userAnswerChoice = answerChoice.target
+    
+    if (userAnswerChoice === questions[index].answer) {
+        console.log('answer is correct')
+    } else {
+        console.log('answer is incorrect')
+    }
+
+}
+
+function startQuiz() {
 // When user clicks on the 'start quiz' button
     // the begin quiz container is hidden.
     beginQuizContainer.classList.add("hide")
@@ -54,10 +68,33 @@ function startQuiz() {
     startTimer()
     
     // one question is shown to the user at a time
-    // when user clicks on an answer choice, 
-        //the choice is evaluated to be correct or incorrect
-        // the user moves on to the next question in the sequence
 
+    questions.forEach((question, index) => {
+        questionsIndex = index
+        questionContainer.innerHTML = `
+        <h2 class="question">${question.question}</h2>
+            <ol class="question-answer-options">
+                <li class="question-answer-choice">${question.options[0]}</li>
+                <li class="question-answer-choice">${question.options[1]}</li>
+                <li class="question-answer-choice">${question.options[2]}</li>
+                <li class="question-answer-choice">${question.options[3]}</li>
+            </ol>
+        `
+    })
+    
+    // when user clicks on an answer choice, 
+    const questionChoices = document.querySelectorAll(".question-answer-choice")
+    console.log(questionChoices)
+    
+    questionChoices.forEach((questionChoice, index) => {
+        questionChoice.addEventListener('click', () => {
+            console.log(questions[questionsIndex].answer)
+
+
+        })
+    })
+    
+    // the user moves on to the next question in the sequence
 
 }
 
